8abfc4813a4cbadebf4e47302126494fa9e25d0b
Humaira Tasnim Adiba <123799736+adibaruet@users.noreply.github.com>
2025-03-06 15:19:38 +0600

    Update hashset.py

--- Contains Dupicate 217/hashset.py
+++ Contains Dupicate 217/hashset.py
@@ -2,10 +2,12 @@
 # Space Complexity: O(n)
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hashset=set()
-        for n in nums:
-            if n in hashset:
-                return True
-            hashset.add(n)
-        return False 
+       h=set()
+       for num in nums:
+        if num in h:
+            return True
+        else: 
+                h.add(num)
+       return False
+        
     
